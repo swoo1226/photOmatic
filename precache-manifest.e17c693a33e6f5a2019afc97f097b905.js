fea82dc929a6022c2b2ff441d6661067b6e97c50
swoo1226 <copycat1226@gmail.com>
2020-03-18 22:19:15 +0900

    Updates

---
+++ precache-manifest.e17c693a33e6f5a2019afc97f097b905.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab66c6866a7f126174cc0c025efe273",
+    "url": "/phot_o_matic/index.html"
+  },
+  {
+    "revision": "c1f8f54ed51ca2bc0b98",
+    "url": "/phot_o_matic/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "211de58e4eb4e9029fb6",
+    "url": "/phot_o_matic/static/js/2.0fb9ee8a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/phot_o_matic/static/js/2.0fb9ee8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1f8f54ed51ca2bc0b98",
+    "url": "/phot_o_matic/static/js/main.935f126b.chunk.js"
+  },
+  {
+    "revision": "8e1da72475ef0c4fdafc",
+    "url": "/phot_o_matic/static/js/runtime-main.d1aedd97.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/phot_o_matic/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/phot_o_matic/precache-manifest.e17c693a33e6f5a2019afc97f097b905.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/phot_o_matic/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
